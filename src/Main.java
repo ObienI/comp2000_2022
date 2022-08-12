d97b7ce47ca2be9a4ca68ee264310fb9424196d6
ObienI <65994570+ObienI@users.noreply.github.com>
2022-08-12 16:25:02 +1000

    Completed Week 2 WorkShop Activity

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
 import javax.swing.*;
-import javax.swing.plaf.DimensionUIResource;
-
 import java.awt.*;
 public class Main extends JFrame {
     public static void main(String[] args) throws Exception{
@@ -9,10 +7,18 @@ public class Main extends JFrame {
 
     class Canvas extends JPanel{
         public Canvas() {
-            setPreferredSize(new Dimension(WIDTH: 720, HEIGHT: 720));
+            setPreferredSize(new Dimension(720, 720));
         }
+        @Override
         public void paint(Graphics g){
-
+            for(int i=10; i<710; i+=35){
+                for (int j = 10; j<710; j+=35){ 
+                    g.setColor(Color.WHITE);
+                    g.fillRect(i, j, 35, 35);
+                    g.setColor(Color.BLACK);
+                    g.drawRect(i, j, 35, 35);    
+                }
+            }
         }
     }
 
@@ -21,7 +27,7 @@ public class Main extends JFrame {
         Canvas canvas = new Canvas();
         this.setContentPane(canvas);
         this.pack();
-        this.setVisible(b: true);
+        this.setVisible(true);
     }
 }
     
